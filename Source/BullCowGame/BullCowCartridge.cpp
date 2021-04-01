898fd7950c9cb0dc7ca47ce8bc4ef74656487fbe
ternaxis <aea5858@gmail.com>
2021-04-02 00:45:11 +0300

    Project finished.
    Unreal engine text based game finished.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,27 +1,182 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
 
     Super::BeginPlay();
-    PrintLine(TEXT("Hi There! Welcome to the Bull and Cow game!"));
-    PrintLine(TEXT("Guess the 4 letter word!"));
-    PrintLine(TEXT("Please enter your input..."));
-    HiddenWord = TEXT("cake");
+
+    
+    
+
+    
+
+    const FString WordListPath = FPaths::ProjectContentDir() / TEXT("WordList/HiddenWordList.txt");
+    FFileHelper::LoadFileToStringArray(words, *WordListPath);
+    
+    
+    Isograms = GetValidWords(words);
+    SetupGame();
+  
+
+    
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {   
-    ClearScreen();
-    
-
-    if(HiddenWord == Input){
-    PrintLine(TEXT("You have won!"));
-    }
-    else
+    if (bGameOver)
     {
-        PrintLine(TEXT("You have lost!"));
-    }
+        ClearScreen();
+        SetupGame();
+    }    
+    else 
+    {
 
+        ProcessGuess(Input);
+       
+
+    }
 }
+
+void UBullCowCartridge::SetupGame()
+{
+    //welcome message
+    PrintLine(TEXT("Hi There! \nWelcome to the Bull and Cow game!"));
+
+    HiddenWord = Isograms[FMath::RandRange(0,Isograms.Num() - 1)];
+    PrintLine(TEXT("%s"), *HiddenWord);
+    lives = HiddenWord.Len()*2;
+    bGameOver = false;
+
+
+   
+    PrintLine(TEXT("Guess the %i letter word!"), HiddenWord.Len());
+    PrintLine(TEXT("You have %i lives."), lives);
+    PrintLine(TEXT("Please enter your input..."));
+
+ 
+    
+}
+
+void UBullCowCartridge::EndGame()
+{
+    bGameOver = true;
+    
+    PrintLine(TEXT("\nPress enter to play again."));
+}
+
+void UBullCowCartridge::ProcessGuess(const FString& Guess)
+{
+ if(HiddenWord == Guess)
+
+        {
+            PrintLine(TEXT("You have won!"));
+            EndGame();
+            return;
+        }
+
+
+       
+
+
+
+        if(HiddenWord.Len() != Guess.Len())
+                {
+                    
+                    PrintLine(TEXT("The Hidden word is %i characters long, you have %i lives remaining. Try again!"), HiddenWord.Len(), --lives);
+                   
+                    
+                }
+
+
+         if(!IsIsogram(Guess)){
+            PrintLine(TEXT("No repeating letters, guess again."));
+            return;
+        }
+
+        PrintLine(TEXT("Lost a life!"));
+        --lives;
+
+
+
+
+
+
+
+            if(lives <= 0)
+            {
+                ClearScreen();
+                PrintLine(TEXT("You have lost!"));
+                PrintLine(TEXT("The hidden word was: %s"), *HiddenWord);
+                EndGame();
+                return;
+            
+            }
+            FBullCowCount Score = GetBullCows(Guess);
+            PrintLine(TEXT("You have %i Bulls and %i cows."),Score.Bulls,Score.Cows);
+
+        PrintLine(TEXT("Guess again, you have %i lives left."), lives);
+}
+
+bool UBullCowCartridge::IsIsogram(const FString& word) const
+{
+
+for(int32 j=0 ; j < word.Len() ; j++){
+
+     for (int32 i = j+1 ; i < word.Len() ; i++)
+    {
+
+        if(word[j]==word[i])
+        return false;
+    }
+}
+
+   return true;
+    
+}
+
+TArray<FString> UBullCowCartridge::GetValidWords(const TArray<FString>& list) const
+{
+
+  TArray<FString> ValidWords;
+    for (FString Word : list){
+
+        if (Word.Len() >=4 && Word.Len() <= 8 && IsIsogram(Word))
+        
+        {
+            
+            
+            ValidWords.Emplace(Word);
+            
+        
+        }
+        
+
+    }
+    return ValidWords;
+}
+
+	FBullCowCount UBullCowCartridge::GetBullCows(const FString& Guess) const
+    {
+        FBullCowCount Count;
+        for(int32 i=0; i<Guess.Len(); i++)
+        {
+            if (Guess[i] == HiddenWord[i])
+            {
+                Count.Bulls++;
+                continue;
+            }
+
+            for(int32 j=0 ; j<HiddenWord.Len(); j++)
+            {
+                if(Guess[i]==HiddenWord[j])
+                {
+                    Count.Cows++;
+                    break;
+                }
+            }
+        }
+        return Count;
+    }
--- Source/BullCowGame/BullCowCartridge.h
+++ Source/BullCowGame/BullCowCartridge.h
@@ -6,7 +6,11 @@
 #include "Console/Cartridge.h"
 #include "BullCowCartridge.generated.h"
 
-
+struct FBullCowCount
+{
+    int32 Bulls = 0;
+    int32 Cows = 0;
+};
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class BULLCOWGAME_API UBullCowCartridge : public UCartridge
@@ -16,10 +20,20 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	public:
 	virtual void BeginPlay() override;
 	virtual void OnInput(const FString& Input) override;
+	void SetupGame();
+	void EndGame();
+	void ProcessGuess(const FString& Guess);
+	bool IsIsogram(const FString& word) const;
+	TArray<FString> GetValidWords(const TArray<FString>& list) const;
+	FBullCowCount GetBullCows(const FString& Guess) const;
 	
 	// Your declarations go below!
 	private:
 	FString HiddenWord;
+	int32 lives;
+	bool bGameOver;
+	TArray<FString> words;
+	TArray<FString> Isograms;
 
 	
 	
